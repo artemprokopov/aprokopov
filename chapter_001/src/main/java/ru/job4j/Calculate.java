4f29c3040a249138058593373ecedeb6871b6288
Artem Prokopov <art1828@mail.ru>
2017-03-08 23:16:39 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -4,11 +4,13 @@ package ru.job4j;
  *Class  Calculate the solution of the task  chapter 001 section 1 task 1.1;
  *@author Prokopov Artem
  *@since 06.03.2017
+ *@version 1.0
  */
 
 public class Calculate {
     /**
      *Main method to output message "Hello World" to the consol;
+     *@param arg - arg;
      */
     public static void main(String[] args) {
 	System.out.println("Hello world.");
