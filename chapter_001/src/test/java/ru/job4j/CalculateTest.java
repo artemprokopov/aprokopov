d3c583960e0c91f300395dd863e5756f4190e204
Artem Prokopov <art1828@mail.ru>
2017-03-09 14:11:39 +0300

    task 4122

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,29 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Artem Prokopov (mailto:art1828@mail.ru)
+* @version $Id$
+* @since 09.03.2017
+* @version 1.0
+*/
+public class CalculateTest {
+/**
+* Test add.
+*/
+    @Test
+    public void whenAddOneToOneThenTwo() {
+	ByteArrayOutputStream out = new ByteArrayOutputStream();
+	System.setOut(new PrintStream(out));
+	Calculate.main(null);
+	assertThat(out.toString(), is(String.format("Hello world.%s",System.getProperty("line.separator"))));
+    }
+}
+ 
