833c4212585c271585f996a9b739f6f7531a1722
Artem Prokopov <art1828@mail.ru>
2017-03-15 01:02:41 +0300

    task 190 fixed

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -23,6 +23,6 @@ public class Max {
      *@return the maximum number.
      */
 	public int max(int first, int second, int third) {
-		return max(max(first, second), max(second, third));
+		return max(max(first, second), third);
 	}
 }
