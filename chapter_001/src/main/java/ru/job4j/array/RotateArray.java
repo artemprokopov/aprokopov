ce27484e84ef5d556850d9231a8093ed5730572e
Artem Prokopov <art1828@mail.ru>
2017-03-18 01:55:08 +0300

    task 223

--- chapter_001/src/main/java/ru/job4j/array/RotateArray.java
+++ chapter_001/src/main/java/ru/job4j/array/RotateArray.java
@@ -21,4 +21,23 @@ public class RotateArray {
 		}
 		return tempArray;
     }
+	/**
+     *Sort the sort method of the array.
+     *@param array - passed the array for rotation.
+     *@return the rotated  array.
+     */
+    public int[][] rotateWithoutAddArray(int[][] array) {
+		int lengthArray = array.length;
+		int temp = 0;
+		for (int i = 0; i < lengthArray / 2; i++) {
+			for (int j = i; j < lengthArray - 1 - i; j++) {
+				temp = array[i][j];
+				array[i][j] = array[lengthArray - 1 - j][i];
+				array[lengthArray - 1 - j][i] = array[lengthArray - 1 - i][lengthArray - 1 - j];
+				array[lengthArray - 1 - i][lengthArray - 1 - j] = array[j][lengthArray - 1 - i];
+				array[j][lengthArray - 1 - i] = temp;
+			}
+		}
+		return array;
+    }
 }
--- chapter_001/src/test/java/ru/job4j/array/RotateArrayTest.java
+++ chapter_001/src/test/java/ru/job4j/array/RotateArrayTest.java
@@ -52,4 +52,46 @@ public class RotateArrayTest {
         int[][] expected = {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
 		assertThat(result, is(expected));
     }
+/**
+     *Test for method "rotateWithoutAddArray".
+     *The tested of the rotation of array clockwise, {{1, 2},{1, 2}}
+     *The exepted result is {{1, 1}, {2, 2}}.
+     */
+    @Test
+    public void whenRotateWithoutAddArray1212ThenArray1122() {
+        int[][] array = {{1, 2}, {1, 2}};
+		RotateArray rotateArray = new RotateArray();
+        int[][] result = rotateArray.rotateWithoutAddArray(array);
+        int[][] expected = {{1, 1}, {2, 2}};
+		assertThat(result, is(expected));
+    }
+	/**
+     *Test for method "rotateWithoutAddArray".
+     *The tested of the rotation of array clockwise, {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}}
+     *The exepted result is {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}}.
+     */
+    @Test
+    public void whenRotateWithoutAddArray123123123ThenArray111222333() {
+        int[][] array = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+		RotateArray rotateArray = new RotateArray();
+		int[][] result = rotateArray.rotateWithoutAddArray(array);
+        int[][] expected = {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
+		assertThat(result, is(expected));
+    }
+	/**
+     *Test for method "rotateWithoutAddArray".
+     *The tested of the rotation of array clockwise, {{1, 2, 3, 4},
+	 *											      {1, 2, 3, 4},
+	 *											      {1, 2, 3, 4},
+	 *                                                {1, 2, 3, 4}}
+     *The exepted result is {{1, 1, 1, 1}, {2, 2, 2, 2}, {3, 3, 3, 3}, {4, 4, 4, 4}}.
+     */
+    @Test
+    public void whenRotateWithoutAddArray1234123412341234ThenArray1111222233334444() {
+        int[][] array = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+		RotateArray rotateArray = new RotateArray();
+        int[][] result = rotateArray.rotateWithoutAddArray(array);
+        int[][] expected = {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
+		assertThat(result, is(expected));
+    }
 }
