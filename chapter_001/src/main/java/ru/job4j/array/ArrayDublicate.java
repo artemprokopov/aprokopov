f19ab6dd02dcff4a3842a6e64d5257167d6be1ad
Smetchik <art1828@mail.ru>
2017-03-20 14:45:57 +0300

    task 225 work

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDublicate.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+/**
+ *Class Arraydublicate demonstrates the array sorting method Bubble.
+ *@author Prokopov Artem
+ *@since 20.03.2017
+ *@version 1.0
+ */
+public class ArrrayDublicate {
+    /**
+     *Sort the sort method of the array.
+     *@param array - passed the array to sort.
+     *@return deployed in the back of the array.
+     */
+	public String[] remove(String[] array) {
+	    String temp = "";
+	    int count = 0;
+	    for (int i = 0; i < array.length - count; i++) {
+		for (int j = i + 1; j < array.length; j++) {
+		    if (array[j].equals(array[i])) {
+			while (array[j].equals(array[array.length - 1 - count]) {
+				count++;	
+			}
+			    temp
+			    }
+		}
+	    }
+	}
+}
