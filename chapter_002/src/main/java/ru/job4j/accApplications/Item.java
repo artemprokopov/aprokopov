817f7b62ecff73dea705528cd78fa538fec4b3b8
Artem Prokopov <art1828@mail.ru>
2017-04-27 00:31:37 +0300

    continued work 1 task 396 #1

--- chapter_002/src/main/java/ru/job4j/accApplications/Item.java
+++ chapter_002/src/main/java/ru/job4j/accApplications/Item.java
@@ -1,21 +1,32 @@
 package ru.job4j.accApplications;
 
+import java.util.Date;
+
 /**
  * Created by Prokopov on 26.04.2017.
  */
 public class Item {
-    private String id;
+    private final String id;
     private String name;
     private String desc;
     private long created;
     private String[] comments;
 
-    public String getId() {
-        return id;
+    public static final Item EMPTY_ITEM;
+    static {
+        EMPTY_ITEM = new Item("empty");
     }
 
-    public void setId(String id) {
-        this.id = id;
+    private Item(String str) {
+        this.id = str;
+    }
+
+    Item() {
+        this.id = Long.toString((new Date().getTime())/(long) (Math.random() * 100))
+                    + "-" + this.hashCode();
+    }
+    public String getId() {
+        return id;
     }
 
     public String getName() {
@@ -48,5 +59,34 @@ public class Item {
 
     public void setComments(String[] comments) {
         this.comments = comments;
+    }
+
+    public boolean equalsId (Item item) {
+        return this.id.equals(item.id);
+    }
+
+    @Override
+    public boolean equals (Object obj) {
+        boolean result = false;
+        if (obj == null || obj.getClass() != this.getClass()) {
+            result = false;
+        } else {
+            Item item = (Item) obj;
+            if (this.id.equals(item.id)
+                    && this.name.equals(item.name)
+                    && this.desc.equals(item.desc)
+                    && this.created == item.created
+                    && this.comments.length == item.comments.length) {
+                for (int i = 0; i < this.comments.length; i++) {
+                    if (this.comments[i].equals(item.comments[i])) {
+                        result = true;
+                    } else {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+        }
+        return  result;
     }
 }
--- chapter_002/src/main/java/ru/job4j/accApplications/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/accApplications/Tracker.java
@@ -5,45 +5,67 @@ package ru.job4j.accApplications;
  */
 public class Tracker {
     private Item[] items;
+    private final static Item[] NULL_ITEM_ARRAY = new  Item[0];
 
     public void add(Item item) throws ArrayIndexOutOfBoundsException {
-        if (this.items != null) {
+        if (this.items != null && item.equals(Item.EMPTY_ITEM)) {
            Item[] temp = new Item[this.items.length + 1];
            System.arraycopy(this.items, 0, temp, 0, this.items.length);
            temp[temp.length - 1] = item;
            this.items = temp;
            temp = null;
-        } else {
+        } else if (!item.equals(Item.EMPTY_ITEM)){
             this.items = new Item[] {item};
         }
     }
 
     public void update(Item item) throws ArrayIndexOutOfBoundsException {
         for (int i = 0; i < items.length; i++) {
-            if (item.getId().equals(items[i].getId())) {
+            if (item.equalsId(this.items[i])) {
                 items[i] = item;
                 break;
             }
         }
     }
 
-    public void delete(Item item) {
+    public void delete(Item item) throws ArrayIndexOutOfBoundsException {
         for (int i = 0; i < items.length; i++) {
-            if (item.getId().equals(items[i].getId())) {
-                items[i] = item;
+            if (item.equals(this.items[i])) {
+                Item[] temp = new Item[this.items.length - 1];
+                System.arraycopy(this.items, 0, temp, 0, i);
+                System.arraycopy(this.items, i + 1, temp, i + 1, this.items.length - (i + 1) - 1);
+                this.items = temp;
+                break;
             }
         }
     }
 
     public Item[] findAll() {
-        return null;
+        return this.items;
     }
 
     public Item[] findByName(String key) {
-        return null;
+        Item[] result = this.NULL_ITEM_ARRAY;
+        for (int i = 0; i < this.items.length; i++) {
+            if (this.items[i].getName().equals(key)) {
+                Item[] temp = new Item[result.length + 1];
+                System.arraycopy(result, 0, temp, 0, result.length);
+                temp[temp.length - 1] = items[i];
+                result = temp;
+            }
+        }
+        return result;
     }
 
-    public void FindById(String id) {
+    public Item FindById(String id) {
+        Item result = Item.EMPTY_ITEM;
+        for (int i = 0; i < this.items.length; i++) {
+            if (this.items[i].getId().equals(id)) {
+                result = this.items[i];
+                break;
+            }
+        }
+        return result;
     }
 
 }
