27c02e2e9e1f6407e1bf0035c4ced512c42acb35
Artem Prokopov <art1828@mail.ru>
2017-05-04 00:31:08 +0300

    task 396 #1

--- chapter_002/src/main/java/ru/job4j/accapplications/AccArray.java
+++ chapter_002/src/main/java/ru/job4j/accapplications/AccArray.java
@@ -85,6 +85,16 @@ public class AccArray<T> {
       }
         return AccArray.EMPTY_ARRAY;
     }
+    
+    public <T> T[] toArray(T[] a) {
+        if (a.length < size)
+            // Make a new array of a's runtime type, but my contents:
+            return (T[]) Arrays.copyOf(array, size, a.getClass());
+        System.arraycopy(array, 0, a, 0, size);
+        if (a.length > size)
+            a[size] = null;
+        return a;
+    }
 
     private void chekSize(int chek) {
         // Проверяем на достижение конца выделенного массива если да, увеличиваем массив
