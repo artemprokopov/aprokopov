6b6948e5047f6f87b80173d62ee0057031908238
Smetchik <art1828@mail.ru>
2017-05-10 12:32:36 +0300

    10_05_2017 task 784 #2

---
+++ chapter_002/src/main/java/ru/job4j/accapplications/Input.java
@@ -0,0 +1,13 @@
+package ru.job4j.accapplications;
+
+/**
+ * Created by Prokopov on 10.05.2017.
+ */
+public interface Input {
+    /**
+     *
+     * @param question параметр запроса.
+     * @return String
+     */
+    String ask(String question);
+}
---
+++ chapter_002/src/main/java/ru/job4j/accapplications/StartUI.java
@@ -0,0 +1,14 @@
+package ru.job4j.accapplications;
+
+/**
+ * Created by Prokopov on 10.05.2017.
+ */
+public class StartUI {
+    /**
+     *
+     * @param args параметры командной строки.
+     */
+    public static void main(String[] args) {
+
+    }
+}
