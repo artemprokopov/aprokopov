752e487906918e07f06856d3204f00381844cad4
smetchik <art1828@mail.ru>
2017-09-15 21:13:07 +0300

    Тестовое задание [#10038]

---
+++ chapter_003/src/main/java/ru/job4j/testtaskcollectionslight/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for test task of th module chapter_003 course job4j.
+ * @author Artem Prokopov
+ * @since 13/09/2017
+ * @version 1.0
+ */
+package ru.job4j.testtaskcollectionslight;
