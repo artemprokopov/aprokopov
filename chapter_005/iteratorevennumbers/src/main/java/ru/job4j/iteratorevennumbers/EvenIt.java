4813d3b2ea451203594832f35b1f2f0e52440c34
smetchik <art1828@mail.ru>
2017-10-12 23:45:14 +0300

    5.1.2. Создать итератор четные числа [#150]

--- chapter_005/iteratorevennumbers/src/main/java/ru/job4j/iteratorevennumbers/EvenIt.java
+++ chapter_005/iteratorevennumbers/src/main/java/ru/job4j/iteratorevennumbers/EvenIt.java
@@ -1,7 +1,52 @@
 package ru.job4j.iteratorevennumbers;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 /**
- *  Iterator class for even numbers. .
+ *  Iterator class for even numbers.
+ *  @author Artem Prokopov
+ *  @version 1.0
+ *  @since 12/10/2017
  */
-public class EvenIt {
+public class EvenIt implements Iterator {
+    /**
+     * Stores numbers array.
+     */
+    private final int[] numbersArray;
+    /**
+     * Store index for next even number, if {@link EvenIt#existNext} = true method ;
+     */
+    private int nextEvenElementArray = 0;
+    private boolean existNext = false;
+
+    public EvenIt(final int[] numbers) {
+        numbersArray = numbers;
+    }
+    @Override
+    public boolean hasNext() {
+        findNextEven();
+        return existNext;
+    }
+
+    private void findNextEven() {
+        for (int i = nextEvenElementArray; i < numbersArray.length && !existNext; i++) {
+            if (numbersArray[i] % 2 == 0) {
+                nextEvenElementArray = i;
+                existNext = true;
+            }
+        }
+    }
+
+    @Override
+    public Integer next() {
+        findNextEven();
+        if(!existNext) {
+            throw new NoSuchElementException("No such element.");
+        }
+        int result = numbersArray[nextEvenElementArray];
+        existNext = false;
+        nextEvenElementArray++;
+        return result;
+    }
 }
