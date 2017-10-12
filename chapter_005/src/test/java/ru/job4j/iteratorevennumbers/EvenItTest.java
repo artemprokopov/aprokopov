c4c4de477f9bd6b9bfd93359585b1db7968587c3
smetchik <art1828@mail.ru>
2017-10-13 00:16:47 +0300

    5.1.2. Создать итератор четные числа [#150]

---
+++ chapter_005/src/test/java/ru/job4j/iteratorevennumbers/EvenItTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.iteratorevennumbers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class EvenItTest {
+    @Test
+    public void hasNext() throws Exception {
+    }
+
+    @Test
+    public void next() throws Exception {
+    }
+
+}
