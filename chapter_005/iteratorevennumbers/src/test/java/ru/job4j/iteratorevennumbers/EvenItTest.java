d002c0453eb00149274a8021d2fc67d77146ea49
smetchik <art1828@mail.ru>
2017-10-12 23:39:26 +0300

    Merge branch iteratorEvenNumbers with master

---
+++ chapter_005/iteratorevennumbers/src/test/java/ru/job4j/iteratorevennumbers/EvenItTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.iteratorevennumbers;
+
+import static org.junit.Assert.*;
+
+public class EvenItTest {
+    @org.junit.Test
+    public void hasNext() throws Exception {
+    }
+
+    @org.junit.Test
+    public void next() throws Exception {
+    }
+
+}
