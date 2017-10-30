b4ebf5a538fdaf0e45dc5a15bb7e7b5cf290360b
Artem Prokopov <art1828@mail.ru>
2017-10-30 14:10:51 +0300

    5.2.1. Реализовать SimpleArray<T> [#156]

--- chapter_005/src/main/java/ru/job4j/simplearray/SimpleArray.java
+++ chapter_005/src/main/java/ru/job4j/simplearray/SimpleArray.java
@@ -1,15 +1,17 @@
 package ru.job4j.simplearray;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class SimpleArray<T> {
-    private final Object[] array;
-    private int currentElement = -1;
+    private static final int ARRAY_INDEX_MAX_VALUE_OFFSET = 5;
+    private static final int ARRAY_MAX_SIZE = Integer.MAX_VALUE - ARRAY_INDEX_MAX_VALUE_OFFSET;
+    private static final int ARRAY_INIT_SIZE = 10;
+    private  Object[] array;
+    private int currentItem = -1;
     private int size;
 
     public SimpleArray() {
-        this(10);
+        this(ARRAY_INIT_SIZE);
     }
 
     public SimpleArray(int initSize) {
@@ -17,81 +19,109 @@ public class SimpleArray<T> {
         this.array = new Object[initSize];
     }
 
-    public boolean add(T addElement) {
-
+    public boolean add(T addItem) {
+        checkAddSizeArray();
+        this.array[++currentItem] = addItem;
         return true;
     }
 
-    public boolean add(int addIndexElement, T addElement) {
-        chekIndex(addIndexElement);
-
-        array[addIndexElement] = addElement;
+    private boolean add(int indexAddItem, T addItem) {
+        chekIndex(indexAddItem);
+        checkAddSizeArray();
+        copyTailArrayWhenAddItem(indexAddItem);
+        currentItem++;
+        array[indexAddItem] = addItem;
         return true;
     }
 
-    public boolean update(int indexUpdateElement, T elementUpdate) {
-        chekIndex(indexUpdateElement);
-        array[indexUpdateElement] = elementUpdate;
+    public boolean update(int indexUpdateItem, T ItemUpdate) {
+        chekIndex(indexUpdateItem);
+        this.array[indexUpdateItem] = ItemUpdate;
         return true;
     }
 
-    public T delete(int indexDeleteElement) {
-        chekIndex(indexDeleteElement);
-        T oldValue = (T) array[indexDeleteElement];
+    public T delete(int indexDeleteItem) {
+        chekIndex(indexDeleteItem);
+        T oldValue = (T) array[indexDeleteItem];
+        copyTailArrayWhenDeletItem(indexDeleteItem);
         return oldValue;
     }
 
-    public T delete(T deleteElement) {
+    public T delete(T deleteItem) {
         T oldValue = null;
-        int indexDeleteElement = findElement(deleteElement);
-        if (indexDeleteElement != -1) {
-            oldValue =(T) array[indexDeleteElement];
-            System.arraycopy(array, indexDeleteElement + 1,
-                    array, indexDeleteElement, currentElement + 1 - indexDeleteElement + 1);
+        int indexDeleteItem = findItem(deleteItem);
+        if (indexDeleteItem != -1) {
+            oldValue = (T) array[indexDeleteItem];
+            copyTailArrayWhenDeletItem(indexDeleteItem);
         }
         return oldValue;
     }
 
-    public T get(int indexElement) {
-        chekIndex(indexElement);
-        return (T) array[indexElement];
+    public T get(int indexItem) {
+        chekIndex(indexItem);
+        return (T) array[indexItem];
     }
 
     public boolean isEmpty() {
-        return currentElement == -1;
+        return this.currentItem == -1;
     }
 
-    public boolean trimArraySize() {
+    public boolean trim() {
+        this.array = Arrays.copyOf(array, this.currentItem + 1);
+        this.size = this.currentItem + 1;
         return true;
     }
 
     private boolean chekIndex(int checkIndex) {
-        if(isEmpty()) {
+        if (isEmpty()) {
             throw new ArrayIndexOutOfBoundsException(this + ": Array empty!");
         }
-        if(checkIndex > currentElement) {
+        if (checkIndex > currentItem) {
             throw new ArrayIndexOutOfBoundsException(checkIndex + " than the last index in the array " + this);
         }
-        if(checkIndex < 0) {
+        if (checkIndex < 0) {
             throw new ArrayIndexOutOfBoundsException(checkIndex + "index less than zero!!!");
         }
         return true;
     }
 
-    private int findElement(T searchElement) {
+    private int findItem(T searchItem) {
         if (isEmpty()) {
             return -1;
         }
-        for (int i = 0; i < currentElement; i++) {
-            if (searchElement.equals((T) array[i])) {
+        for (int i = 0; i < currentItem; i++) {
+            if (searchItem.equals((T) array[i])) {
                 return i;
             }
         }
         return -1;
     }
 
-    private boolean ceckAddSizeArray() {
+    private boolean checkAddSizeArray() {
+        if (currentItem + 1 >= size) {
+            size = checkMaxSizeArray();
+            array = Arrays.copyOf(array, size);
+        }
+        return true;
+    }
 
+    private int checkMaxSizeArray() {
+        if (ARRAY_MAX_SIZE - (currentItem + 1) == 0) {
+            throw new OutOfMemoryError("The array index is greater than the maximum possible values");
+        }
+        return ARRAY_MAX_SIZE - size > size
+                ? size =+ size : ARRAY_MAX_SIZE;
+    }
+
+    private boolean copyTailArrayWhenDeletItem(int indexDeleteItem) {
+        System.arraycopy(array, indexDeleteItem + 1,
+                array, indexDeleteItem, currentItem + 1 - indexDeleteItem + 1);
+        return true;
+    }
+
+    private boolean copyTailArrayWhenAddItem(int indexAddItem) {
+        System.arraycopy(array, indexAddItem,
+                array, indexAddItem + 1, currentItem + 1 - indexAddItem + 1);
         return true;
     }
 }
